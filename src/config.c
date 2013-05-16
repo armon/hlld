fcc55f3870ba0fd4c535a396044546d9a327ef55
Armon Dadgar <armon.dadgar@gmail.com>
2013-05-16 15:14:08 -0700

    Config properly computes eps given precision and visa versa

--- src/config.c
+++ src/config.c
@@ -97,7 +97,7 @@ static int config_callback(void* user, const char* section, const char* name, co
     hlld_config *config = (hlld_config*)user;
 
     // Switch on the config
-    #define NAME_MATCH(param) (strcasecmp(param, name) == 0)
+#define NAME_MATCH(param) (strcasecmp(param, name) == 0)
 
     // Handle the int cases
     if (NAME_MATCH("port")) {
@@ -107,29 +107,39 @@ static int config_callback(void* user, const char* section, const char* name, co
     } else if (NAME_MATCH("udp_port")) {
         return value_to_int(value, &config->udp_port);
     } else if (NAME_MATCH("flush_interval")) {
-         return value_to_int(value, &config->flush_interval);
+        return value_to_int(value, &config->flush_interval);
     } else if (NAME_MATCH("cold_interval")) {
-         return value_to_int(value, &config->cold_interval);
+        return value_to_int(value, &config->cold_interval);
     } else if (NAME_MATCH("in_memory")) {
-         return value_to_int(value, &config->in_memory);
+        return value_to_int(value, &config->in_memory);
     } else if (NAME_MATCH("use_mmap")) {
-         return value_to_int(value, &config->use_mmap);
+        return value_to_int(value, &config->use_mmap);
     } else if (NAME_MATCH("workers")) {
-         return value_to_int(value, &config->worker_threads);
+        return value_to_int(value, &config->worker_threads);
     } else if (NAME_MATCH("default_precision")) {
-         return value_to_int(value, &config->default_precision);
+        int res = value_to_int(value, &config->default_precision);
+        if (res) {
+            // Compute error given precision
+            config->default_eps = hll_error_for_precision(config->default_precision);
+        }
+        return res;
 
-    // Handle the double cases
+        // Handle the double cases
     } else if (NAME_MATCH("default_eps")) {
-         return value_to_double(value, &config->default_eps);
+        int res = value_to_double(value, &config->default_eps);
+        if (res) {
+            // Compute precision given error
+            config->default_precision = hll_precision_for_error(config->default_eps);
+        }
+        return res;
 
-    // Copy the string values
+        // Copy the string values
     } else if (NAME_MATCH("data_dir")) {
         config->data_dir = strdup(value);
     } else if (NAME_MATCH("log_level")) {
         config->log_level = strdup(value);
 
-    // Unknown parameter?
+        // Unknown parameter?
     } else {
         // Log it, but ignore
         syslog(LOG_NOTICE, "Unrecognized config parameter: %s", value);
@@ -196,7 +206,7 @@ int sane_data_dir(char *data_dir) {
     if (res == 0) {
         if ((buf.st_mode & S_IFDIR) == 0) {
             syslog(LOG_ERR,
-                   "Provided data directory exists and is not a directory!");
+                    "Provided data directory exists and is not a directory!");
             return 1;
         }
     } else  {
@@ -204,7 +214,7 @@ int sane_data_dir(char *data_dir) {
         res = mkdir(data_dir, 0775);
         if (res != 0) {
             syslog(LOG_ERR,
-                   "Failed to make the data directory! Err: %s", strerror(errno));
+                    "Failed to make the data directory! Err: %s", strerror(errno));
             return 1;
         }
     }
@@ -221,7 +231,7 @@ int sane_data_dir(char *data_dir) {
     // If we failed to open the file, error
     if (fh == -1) {
         syslog(LOG_ERR,
-               "Failed to write to data directory! Err: %s", strerror(errno));
+                "Failed to write to data directory! Err: %s", strerror(errno));
         return 1;
     }
 
@@ -229,7 +239,7 @@ int sane_data_dir(char *data_dir) {
 }
 
 int sane_log_level(char *log_level, int *syslog_level) {
-    #define LOG_MATCH(lvl) (strcasecmp(lvl, log_level) == 0)
+#define LOG_MATCH(lvl) (strcasecmp(lvl, log_level) == 0)
     if (LOG_MATCH("DEBUG")) {
         *syslog_level = LOG_UPTO(LOG_DEBUG);
     } else if (LOG_MATCH("INFO")) {
@@ -250,7 +260,7 @@ int sane_log_level(char *log_level, int *syslog_level) {
 int sane_default_eps(double eps) {
     if (eps >= 0.3) {
         syslog(LOG_ERR,
-               "Epsilon cannot be greater than 0.3!");
+                "Epsilon cannot be greater than 0.3!");
         return 1;
 
     } else if (eps < 0.0021) {
@@ -266,7 +276,7 @@ int sane_default_eps(double eps) {
 int sane_default_precision(int precision) {
     if (precision < HLL_MIN_PRECISION) {
         syslog(LOG_ERR,
-               "Precision cannot be less than %d!", HLL_MIN_PRECISION);
+                "Precision cannot be less than %d!", HLL_MIN_PRECISION);
         return 1;
     } else if (precision > HLL_MAX_PRECISION) {
         syslog(LOG_ERR, "Precision cannot be more than %d!", HLL_MAX_PRECISION);
@@ -282,13 +292,13 @@ int sane_default_precision(int precision) {
 int sane_flush_interval(int intv) {
     if (intv == 0) {
         syslog(LOG_WARNING,
-               "Flushing is disabled! Increased risk of data loss.");
+                "Flushing is disabled! Increased risk of data loss.");
     } else if (intv < 0) {
         syslog(LOG_ERR, "Flush interval cannot be negative!");
         return 1;
     } else if (intv >= 600)  {
         syslog(LOG_WARNING,
-               "Flushing set to be very infrequent! Increased risk of data loss.");
+                "Flushing set to be very infrequent! Increased risk of data loss.");
     }
     return 0;
 }
@@ -296,13 +306,13 @@ int sane_flush_interval(int intv) {
 int sane_cold_interval(int intv) {
     if (intv == 0) {
         syslog(LOG_WARNING,
-               "Cold data unmounting is disabled! Memory usage may be high.");
+                "Cold data unmounting is disabled! Memory usage may be high.");
     } else if (intv < 0) {
         syslog(LOG_ERR, "Cold interval cannot be negative!");
         return 1;
     } else if (intv < 300) {
         syslog(LOG_ERR, "Cold interval is less than 5 minutes. \
-This may cause excessive unmapping to occur.");
+                This may cause excessive unmapping to occur.");
     }
 
     return 0;
@@ -311,11 +321,11 @@ This may cause excessive unmapping to occur.");
 int sane_in_memory(int in_mem) {
     if (in_mem != 0) {
         syslog(LOG_WARNING,
-               "Default sets are in-memory only! Sets not persisted by default.");
+                "Default sets are in-memory only! Sets not persisted by default.");
     }
     if (in_mem != 0 && in_mem != 1) {
         syslog(LOG_ERR,
-               "Illegal value for in-memory. Must be 0 or 1.");
+                "Illegal value for in-memory. Must be 0 or 1.");
         return 1;
     }
 
@@ -325,11 +335,11 @@ int sane_in_memory(int in_mem) {
 int sane_use_mmap(int use_mmap) {
     if (use_mmap != 1) {
         syslog(LOG_WARNING,
-               "Without use_mmap, a crash of hlld can result in data loss.");
+                "Without use_mmap, a crash of hlld can result in data loss.");
     }
     if (use_mmap != 0 && use_mmap != 1) {
         syslog(LOG_ERR,
-               "Illegal value for use_mmap. Must be 0 or 1.");
+                "Illegal value for use_mmap. Must be 0 or 1.");
         return 1;
     }
     return 0;
@@ -338,7 +348,7 @@ int sane_use_mmap(int use_mmap) {
 int sane_worker_threads(int threads) {
     if (threads <= 0) {
         syslog(LOG_ERR,
-               "Cannot have fewer than one worker thread!");
+                "Cannot have fewer than one worker thread!");
         return 1;
     }
     return 0;
@@ -384,23 +394,23 @@ static int set_config_callback(void* user, const char* section, const char* name
     hlld_set_config *config = (hlld_set_config*)user;
 
     // Switch on the config
-    #define NAME_MATCH(param) (strcasecmp(param, name) == 0)
+#define NAME_MATCH(param) (strcasecmp(param, name) == 0)
 
     // Handle the int cases
     if (NAME_MATCH("in_memory")) {
-         return value_to_int(value, &config->in_memory);
+        return value_to_int(value, &config->in_memory);
     } else if (NAME_MATCH("default_precision")) {
-         return value_to_int(value, &config->default_precision);
+        return value_to_int(value, &config->default_precision);
 
-    // Handle big int
+        // Handle big int
     } else if (NAME_MATCH("size")) {
-         return value_to_int64(value, &config->size);
+        return value_to_int64(value, &config->size);
 
-    // Handle the double cases
+        // Handle the double cases
     } else if (NAME_MATCH("default_eps")) {
-         return value_to_double(value, &config->default_eps);
+        return value_to_double(value, &config->default_eps);
 
-    // Unknown parameter?
+        // Unknown parameter?
     } else {
         // Log it, but ignore
         syslog(LOG_NOTICE, "Unrecognized set config parameter: %s", value);
@@ -445,14 +455,14 @@ int update_filename_from_set_config(char *filename, hlld_set_config *config) {
 
     // Write out
     fprintf(f, "[hlld]\n\
-size = %llu\n\
-default_eps = %f\n\
-default_precision = %d\n\
-in_memory = %d\n", (unsigned long long)config->size,
-                 config->default_eps,
-                 config->default_precision,
-                 config->in_memory
-    );
+            size = %llu\n\
+            default_eps = %f\n\
+            default_precision = %d\n\
+            in_memory = %d\n", (unsigned long long)config->size,
+            config->default_eps,
+            config->default_precision,
+            config->in_memory
+           );
 
     // Close
     fclose(f);
